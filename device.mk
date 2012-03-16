bac8226f4c67aa1fc519b7dc02add6b3cd2c2001
Hugo Dupras <h-dupras@ti.com>
2012-03-16 14:28:28 +0100

    Blaze: Add permissions for location.gps
    Set the right permissions for activate location via gps
    
    Change-Id: I1f967540ed20a4afe53cdf9989d10c12c7b74210
    Signed-off-by: Hugo Dupras <h-dupras@ti.com>

--- device.mk
+++ device.mk
@@ -48,6 +48,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
 	frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+	frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
 	frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
 	frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
 	frameworks/base/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
