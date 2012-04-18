96e47d594ae230b56d55b8788764ec8675df903b
Yuriy Zabroda <yuriy.zabroda@ti.com>
2012-04-18 12:36:01 +0300

    CTS failire on SystemFeaturesTest.testSensorFeatures is fixed
    An additional feature is added to device/ti/blaze/device.mk:
       * android.hardware.sensor.compass.xml
    
    Change-Id: If263793f47dfa578fe026735076ccf1916665f71
    Signed-off-by: Yuriy Zabroda <yuriy.zabroda@ti.com>

--- device.mk
+++ device.mk
@@ -46,6 +46,7 @@ PRODUCT_COPY_FILES += \
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
+	frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
 	frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
 	frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
 	frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
