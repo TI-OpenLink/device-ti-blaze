3c0c61e22a332a407b5a5e6bcf3a64cba2afc635
Yuriy Zabroda <yuriy.zabroda@ti.com>
2012-04-18 13:45:26 +0300

    CTS failure on SystemFeaturesTest.testScreenFeatures is fixed
    An additional feature is added to device/ti/blaze/device.mk:
       * handheld_core_hardware.xml
    
    Change-Id: Iefb914399dfb52ad746f6c32b6ca9d8668fedc94
    Signed-off-by: Yuriy Zabroda <yuriy.zabroda@ti.com>

--- device.mk
+++ device.mk
@@ -51,6 +51,7 @@ PRODUCT_COPY_FILES += \
 	frameworks/base/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
 	frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
 	frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+	frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
 	frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
 	frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
 	frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
