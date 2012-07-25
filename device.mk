58eb1f8fb09efbd271d5a8f147cef41670a91ba0
Sergii Iegorov <x0155539@ti.com>
2012-07-25 12:41:11 -0500

    Adding audio policy file to installed files.
    This is a part of porting ICS Audio HAL to JB.
    Audio policy file is new one introduced in JB.
    
    Change-Id: I9dec1587129f999371e9001fc7882e01889ea572
    Signed-off-by: Sergii Iegorov <x0155539@ti.com>

--- device.mk
+++ device.mk
@@ -36,6 +36,7 @@ PRODUCT_COPY_FILES := \
 	device/ti/blaze/omap4-keypad.kl:system/usr/keylayout/omap4-keypad.kl \
 	device/ti/blaze/omap4-keypad.kcm:system/usr/keychars/omap4-keypad.kcm \
         device/ti/blaze/twl6030_pwrbutton.kl:system/usr/keylayout/twl6030_pwrbutton.kl \
+	device/ti/common-open/audio/audio_policy.conf:system/etc/audio_policy.conf \
 	device/ti/blaze/media_profiles.xml:system/etc/media_profiles.xml \
         device/ti/blaze/media_codecs.xml:system/etc/media_codecs.xml \
         device/ti/blaze/syn_tm12xx_ts_1.idc:system/usr/idc/syn_tm12xx_ts_1.idc \
