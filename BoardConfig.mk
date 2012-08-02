a3d41788ce3bb2780f8203d64daab8c04bf5fa66
Ido Reis <idor@ti.com>
2012-08-02 18:02:11 +0300

    replace the wl12xx with wlcore
    Change-Id: I3cadcc0b25eccbf65245a9d0bc7610e9034eb882
    Signed-off-by: Ido Reis <idor@ti.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,8 +85,8 @@ BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB        :=
 BOARD_WLAN_DEVICE                := wl12xx_mac80211
 BOARD_SOFTAP_DEVICE              := wl12xx_mac80211
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx_sdio.ko"
-WIFI_DRIVER_MODULE_NAME          := "wl12xx_sdio"
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wlcore_sdio.ko"
+WIFI_DRIVER_MODULE_NAME          := "wlcore_sdio"
 WIFI_FIRMWARE_LOADER             := ""
 COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
 endif
